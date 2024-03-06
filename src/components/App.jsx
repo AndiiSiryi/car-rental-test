10dd617a396a71c2be1e5c067cb98e39fd8e7d19
AndiiSiryi <130078717+AndiiSiryi@users.noreply.github.com>
2024-03-06 12:00:45 +0200

    started the project

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import css from './App.module.css';
+
+const App = () => {
+  return (
+    <div className={css.styleApp}>
+      <h1>car-rental-test</h1>
+    </div>
+  );
+};
+export default App;
