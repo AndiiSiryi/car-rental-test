474e7b14505fb0a72c98bb6981454d66d2a5ecac
AndiiSiryi <130078717+AndiiSiryi@users.noreply.github.com>
2024-03-06 12:09:45 +0200

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,8 @@ import css from './App.module.css';
 const App = () => {
   return (
     <div className={css.styleApp}>
-      <h1>car-rental-test</h1>
+      <h1>car-rental-test1</h1>
+      <h2>car-rental-test1</h2>
     </div>
   );
 };
